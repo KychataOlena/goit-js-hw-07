e0d20e505d7c91989e772a877840bc358e34f4e7
KychataOlena <106249466+KychataOlena@users.noreply.github.com>
2022-10-15 22:34:37 +0300

    01-gallery.js
    без бібліотеки

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const imagesContainer = document.querySelector(".gallery");
+const cardsMarkup = createGalleryCardsMurkup(galleryItems);
 
-console.log(galleryItems);
+imagesContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+imagesContainer.addEventListener("click", onImagesContainerClick);
+
+// console.log(galleryItems);
+function createGalleryCardsMurkup(images) {
+    return images.map(({ preview, original, description }) => {
+        return `
+     <div class="gallery__item">
+    <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    }).join("");
+   
+}
+
+function onImagesContainerClick(evt) {
+    if (evt.target.nodeName !== 'IMG') {
+        return
+    }
+    // console.log(evt.target);
+}
