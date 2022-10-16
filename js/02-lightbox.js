ac49f1d3b71ede9f5284cf7ca4ead92f9ebfcd71
KychataOlena <106249466+KychataOlena@users.noreply.github.com>
2022-10-16 22:39:07 +0300

    виконано 02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
-import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
+import { galleryItems } from "./gallery-items.js";
+
+const imagesContainer = document.querySelector(".gallery");
+const cardsMarkup = createGalleryCardsMurkup(galleryItems);
+// console.log(cardsMarkup);
+
+imagesContainer.insertAdjacentHTML("beforeend", cardsMarkup);
+
+// console.log(galleryItems);
+function createGalleryCardsMurkup(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `
+    <a class="gallery__item" 
+    href="${original}">
+  <img class="gallery__image" 
+  src="${preview}" 
+  alt="${description}" />
+</a>`;
+    })
+    .join("");
+}
+
+const lightboxSettings = {
+    captions: true,
+  captionsData: "alt",
+  captionDelay: 250,
+}
+const lightbox = new SimpleLightbox(".gallery a", lightboxSettings);
+
 
 console.log(galleryItems);
